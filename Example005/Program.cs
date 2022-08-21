8cdbd02cc2ba670b149545df6fb0605ffdea973d
Ne-eK <kartsan.nikolay@yandex.ru>
2022-08-21 11:01:41 +0300

    Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(1, 100);
+double numberB = new Random().Next(1, 101);;
+double Res = numberA / numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(Res);
