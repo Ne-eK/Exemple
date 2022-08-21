1b4cc2ba41b7e9c286a98ee370358847f9ff410c
Ne-eK <kartsan.nikolay@yandex.ru>
2022-08-21 11:29:19 +0300

    Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string UserName = Console.ReadLine();
+
+if (UserName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, этоже Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(UserName);
+}
